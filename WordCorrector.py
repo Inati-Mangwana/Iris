e3c4d1ed15076c1eef7520d0b400ac3fd308b63f
Inati Mangwana (Intern) <inati.mangwana@clickatell.com>
2022-09-29 10:44:58 +0200

    WC

---
+++ WordCorrector.py
@@ -0,0 +1,14 @@
+
+from textblob import Word
+
+
+def correct_word_spelling(word):
+    
+    word = Word(word)
+    
+    result = word.correct()
+    
+    print(result)
+    
+
+correct_word_spelling('appple')
